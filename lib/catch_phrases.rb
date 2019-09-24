013b6678be2024e8fa0eb05527b8184eef1d795b
William <teststuffreally@gmail.com>
2019-09-24 14:23:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
 def say It's-a me Mario!  # remove
   message="It's-a me Mario! # and
+  
   method="It's-a me Mario!"
   status='Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
